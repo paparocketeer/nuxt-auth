f51189c84ffae0afd5e2186729a1e416d0e95d87
paparocketeer <paparocketeer@gmail.com>
2020-09-28 20:42:13 +0300

    finish

--- store/index.js
+++ store/index.js
@@ -1,7 +1,5 @@
 const cookieparser = process.server ? require('cookieparser') : undefined
-const config = require('../nuxt.config')
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('cbbeff8dfdcd48468b2d02bf413031af');
+const NewsAPI = require('newsapi')
 
 export const state = () => ({
   news: [],
@@ -12,31 +10,31 @@ export const mutations = {
   updateNews(state, posts) {
     state.news = posts
   },
-  setAuth (state, auth) {
+  setAuth(state, auth) {
     state.auth = auth
-  }
+  },
 }
 
-
 export const actions = {
-  async fetchPosts({ commit }) {
-    const posts = await this.$axios.$get(
-      `https://newsapi.org/v2/everything?q=apple&from=2020-09-27&to=2020-09-27&sortBy=popularity&apiKey=${ this.$config.apiSecret }`
-    )
-    if (!config.dev) {
-      newsapi.v2.everything({
+  fetchPosts({ commit }) {
+    // const posts = await this.$axios.$get(
+    //   `https://newsapi.org/v2/everything?q=apple&from=2020-09-27&to=2020-09-27&sortBy=popularity&apiKey=${ this.$config.apiSecret }`
+    // )
+    const newsapi = new NewsAPI(this.$config.apiSecret, {
+      corsProxyUrl: 'https://cors-anywhere.herokuapp.com/',
+    })
+    newsapi.v2
+      .everything({
         q: 'apple',
         from: '2020-09-27',
         to: '2020-09-27',
         sortBy: 'popularity',
-      }).then(response => {
-        posts = response
-      });
-    }
-    
-    commit('updateNews', posts.articles.slice(0, 10))
+      })
+      .then((response) => {
+        commit('updateNews', response.articles.slice(0, 10))
+      })
   },
-  nuxtServerInit ({ commit }, { req }) {
+  nuxtServerInit({ commit }, { req }) {
     let auth = null
     if (req.headers.cookie) {
       const parsed = cookieparser.parse(req.headers.cookie)
